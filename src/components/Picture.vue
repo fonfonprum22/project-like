80b201867a387e93d109319d63070f0471db67f7
unknown <fon0948630361@gmail.com>
2020-08-22 21:30:28 +0700

    Up

--- src/components/Picture.vue
+++ src/components/Picture.vue
@@ -1,76 +1,71 @@
 <template>
- <div class="container" id="picture">
-     <div class="card">
-          <p class="card-text 
-                    d-flex justify-content-end 
-                    p-3">
-                Counter: {{counter}}
-            </p>
-<!-- Image -->
-            <img 
-                :src="require(`../assets/img/${image}.jpg`)"
-                class="card-img-top mx-auto picture" 
-                style="width:400px">
+  <div class="container" id="picture">
+    <div class="card">
+      <p class="card-text d-flex justify-content-end p-3">Counter: {{counter}}</p>
+      <!-- Image -->
+      <img
+        :src="require(`../assets/img/${image}.jpg`)"
+        class="card-img-top mx-auto picture"
+        style="width:400px"
+      />
 
-        <div class="card-body">
-<!-- Couter -->
-           
-        </div>
-<!-- Button footer -->
-        <div class="d-flex justify-content-center">
-            <ul class="list-inline">
-            <li v-for="btn in buton" 
-                v-bind:key="btn.id" 
-                class="list-inline-item">
-                <ButtonAction 
-                    :data = "btn.name" 
-                    :couter = btn.couter
-                    v-on:toParent="onClick"
-                    />
-            </li>
-            </ul>
-        </div>
-<!-- end button footer -->
-     </div>
- </div> 
+      <div class="card-body">
+        <!-- Couter -->
+      </div>
+      <!-- Button footer -->
+      <div class="d-flex justify-content-center">
+        <ul class="list-inline">
+          <li v-for="btn in buton" v-bind:key="btn.id" class="list-inline-item">
+            <ButtonAction :data="btn.name" :couter="btn.couter" v-on:toParent="onClick" />
+          </li>
+        </ul>
+      </div>
+      <!-- end button footer -->
+    </div>
+  </div>
 </template>
 
 <script>
-import ButtonAction from './Button'
+import ButtonAction from "./Button";
 
 export default {
-    components: {
-        ButtonAction
-        },
-    data () {
-        return {
-            buton : [
-                { name:"like", couter:1 },
-                { name:"love", couter:10},
-                { name:"haha", couter:5},
-                { name:"wow", couter:20},
-                { name:"sad", couter:-5},
-                { name:"angry", couter:-20}
-                ],
-            counter:0
-        }
+  components: {
+    ButtonAction,
+  },
+  data() {
+    return {
+      buton: [
+        { name: "like", couter: 1 },
+        { name: "love", couter: 10 },
+        { name: "haha", couter: 5 },
+        { name: "wow", couter: 20 },
+        { name: "sad", couter: -5 },
+        { name: "angry", couter: -50 },
+      ],
+      counter: 0,
+    };
+  },
+  props: ["image"],
+  methods: {
+    onClick(value) {
+      this.counter = this.counter + value;
     },
-    props : ['image'],
-    methods: {
-        onClick (value) {
-            this.counter = this.counter + value
-            }
-        }
-}
+  },
+};
 </script>
 
 <style>
-
-#picture{
-    padding: 20px;
-    }
+#picture {
+  padding: 20px;
+}
 .picture {
-    border-radius: 20px;
-    }
-
+  border-radius: 20px;
+  width: 80%;
+  background-color: white;
+  box-shadow: 0 4px 70px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+}
+.card {
+  background-color: rgb(245, 195, 203);
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+}
 </style>
--- src/components/Slider.vue
+++ src/components/Slider.vue
@@ -1,8 +1,9 @@
 <template>
     
 <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-  <div class="carousel-inner" >
-      
+  
+  <div class="carousel-inner" id="img">
+      <p>Project-Like</p>
         <div class="carousel-item active">
 <!-- Image Slide 01 -->
         <img class="d-block w-100" src="../assets/img/sea1.jpg" >
@@ -12,14 +13,16 @@
             <img v-bind:src="require(`../assets/img/${image.url}`)" class="d-block w-100" alt="...">
         </div>
   </div>
-  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+ 
+  <a  class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
     <span class="sr-only">Previous</span>
   </a>
-  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+  <a  class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span id="button" class="carousel-control-next-icon" aria-hidden="true"></span>
     <span class="sr-only">Next</span>
   </a>
+
 </div>
 
 </template>
@@ -36,3 +39,20 @@ export default {
         }
 }
 </script>
+
+<style>
+#img{
+  
+  background-color: white;
+  box-shadow: 0 10px 100px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+ border-radius: 30px;
+}
+#carouselExampleControls{
+  display: flex;
+  justify-content: center;
+}
+#button{
+  color: black;
+}
+
+</style>
